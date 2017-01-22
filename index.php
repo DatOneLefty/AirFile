ee5a1a96431d799f76c489704bf3f35da5e7f67a
DatOneLefty <k9dogs101@gmail.com>
2017-01-22 14:00:50 -0500

    modified:   index.php

--- index.php
+++ index.php
@@ -62,6 +62,8 @@ echo "<img src='icns/back.png' width='20px' height='15px'></img>&#160;&#160;&#16
 $to = $_GET['FS'] .  "/..";
 echo("<tr><td></td><td><img src='icns/back.png' width='20px' height='15px'></img>&#160;&#160;&#160;&#160;&#160;<a class='back' href='index.php?FS=$to'>Back</a></td><td>" ."</td><td>" ."</td></tr>");
 $i = 0;
+$f = 0;
+$d = 0;
 foreach($files as $file) {
 	$i++;
 	if (is_readable($_GET['FS'] . "/" . $file)) {
@@ -71,6 +73,7 @@ foreach($files as $file) {
 	}
   if (is_dir($_GET['FS'] . "/" . $file)) {
 	$fmt = $_GET['FS'] . "/" . $file;
+	$d++;
 	if ($read == True) {
 	echo("<tr id='$i'><td></td><td><img src='icns/folder.png' width='20px' height='15px'></img>&#160;&#160;&#160;&#160;&#160;<a class='folder' href='index.php?FS=$fmt'>$file</a></td><td>" . date ("F d Y H:i:s", filemtime($_GET['FS'] . "/" . $file)) .  "</td><td>Folder</td></tr>");
 
@@ -80,14 +83,20 @@ foreach($files as $file) {
 	}
 	} else {
 	$icon = findend($file);
+	$f++;
 	if ($read == True) {
-  echo("<tr id='$i' onclick='select(" . '"' . $i .  '"' .");' style='old'><td></td><td>&#160;<img src='icns/$icon' width='15px' height='20px'></img>&#160;&#160;&#160;&#160;&#160;<a class='file'>$file</a></td><td>" . date ("F d Y H:i:s", filemtime($_GET['FS'] . "/" . $file)) .  "</td><td>" . formatBytes(filesize($_GET['FS'] . "/" . $file)) ."</td></tr>");
+	$size = filesize($_GET['FS'] . "/" . $file);
+	$totalsize = $totalsize + $size;
+  echo("<tr id='$i' onclick='select(" . '"' . $i .  '"' .");' style='old'><td></td><td>&#160;<img src='icns/$icon' width='15px' height='20px'></img>&#160;&#160;&#160;&#160;&#160;<a class='file'>$file</a></td><td>" . date ("F d Y H:i:s", filemtime($_GET['FS'] . "/" . $file)) .  "</td><td>" . formatBytes($size) ."</td></tr>");
 	} else {
   echo("<tr id='$i' onclick='select(" . '"' . $i .  '"' .");' style='old'><td></td><td>&#160;<img src='icns/$icon' width='15px' height='20px'></img>&#160;&#160;&#160;&#160;&#160;<a class='unre'>$file</a></td><td>" . date ("F d Y H:i:s", filemtime($_GET['FS'] . "/" . $file)) .  "</td><td>" . formatBytes(filesize($_GET['FS'] . "/" . $file)) ."</td></tr>");
 	}
 	}
 }
 echo '</table>';
+echo "<br><center>a total of $f files taking up " . formatBytes($totalsize) . "(excluding unreadable files)";
+echo "<br>There is also $d folders";
+echo "<br> In total there are " . ($f + $d) . " files and directories</center>";
 ?>
 <?php
 }
