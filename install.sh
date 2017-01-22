4e149be8fe99a363750368d0bce5dafadf608e47
DatOneLefty <k9dogs101@gmail.com>
2017-01-21 19:51:22 -0500

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 echo "copying files"
-sudo cp ../OnlineFiles /var
+sudo cp -r ../OnlineFiles /var/www
 sudo chmod 755 ~/.profile
-sudo chmod 755 /etc/apache/sites-enabled/000-default.conf
+sudo chmod 777 /etc/apache2/sites-enabled/000-default.conf
 
 
 echo "adding files to path"
-sudo echo "export PATH=$PATH:/var/OnlineFiles/bin" >> ~/.profile
+sudo echo 'PATH="$PATH:/var/www/Online/Files/bin"' >> ~/.profile
 echo "Adding data to apache"
 echo "Listen 25500" >> /etc/apache2/sites-enabled/000-default.conf
 echo "<VirtualHost *:25500>" >> /etc/apache2/sites-enabled/000-default.conf
 echo "	ServerAdmin webmaster@localhost" >> /etc/apache2/sites-enabled/000-default.conf
-echo "	DocumentRoot /var/OnlineFiles" >> /etc/apache2/sites-enabled/000-default.conf
+echo "	DocumentRoot /var/www/OnlineFiles" >> /etc/apache2/sites-enabled/000-default.conf
 echo "	ErrorLog ${APACHE_LOG_DIR}/error.log" >> /etc/apache2/sites-enabled/000-default.conf
 echo "	CustomLog ${APACHE_LOG_DIR}/access.log combined" >> /etc/apache2/sites-enabled/000-default.conf
 echo "</VirtualHost>" >> /etc/apache2/sites-enabled/000-default.conf
 sudo service apache2 restart
 echo "Finished installation"
+sudo chown -R $USER:$USER /var/www/OnlineFiles
+find /var/www/OnlineFiles -type d -exec chmod 777 {} \;
+
+
